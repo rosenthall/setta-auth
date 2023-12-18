68bc0b767cd4372a77888e464156939f1d585e51
armagg <rosenthall@riseup.net>
2023-12-18 17:34:57 +0200

    improved GenerateToken, now generating logic is in separate function

--- internal/services/auth/generate.go
+++ internal/services/auth/generate.go
@@ -28,67 +28,66 @@ func (s *JwtAuthService) GenerateToken(ctx context.Context, in *pb.GenerateToken
 		return nil, status.Errorf(codes.InvalidArgument, "user_id is empty")
 	}
 
+	// Generate access and refresh tokens
+	accessToken, refreshToken, err := s.generateTokens(ctx, in.UserId, in.UserData)
+	if err != nil {
+		return nil, err
+	}
+
+	s.log.Debug("Tokens generated successfully")
+
+	// Sending the response
+	return &pb.TokenResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
+
+// generateTokens creates new access and refresh tokens
+func (s *JwtAuthService) generateTokens(ctx context.Context, userId string, userData *pb.UserData) (string, string, error) {
 	// Adding user_id to claims
 	claims := jwt.MapClaims{
-		"user_id": in.UserId,
+		"user_id": userId,
 	}
 
 	// Adding additional user data
-	for key, value := range in.UserData.AdditionalData {
+	for key, value := range userData.AdditionalData {
 		claims[key] = value
 	}
-
-	s.log.Debug("User data added to token claims")
 
 	// Adding exp field to the claims
 	jwtTTL := s.tokenTTL
 	jwtExpiresAt := time.Now().Add(jwtTTL)
-	claims["exp"] = jwtExpiresAt.Unix() + 100000000000
-	//Creating the token
+	claims["exp"] = jwtExpiresAt.Unix()
+
+	// Creating the access token
 	token := jwt.NewWithClaims(s.signingMethod, claims)
-	// Signing the token
 	signedToken, err := token.SignedString(s.privateKey)
 	if err != nil {
-		s.log.Errorf("Failed to sign token: %v", err)
-		return nil, status.Errorf(codes.Internal, "failed to sign token")
+		s.log.Errorf("Failed to sign access token: %v", err)
+		return "", "", status.Errorf(codes.Internal, "failed to sign access token")
 	}
 
-	s.log.Debug("Token signed successfully")
-
 	// Generating the refresh token
-	// We use random hex-string with length 36 bytes.
 	refreshToken, err := generateRandomString(36)
 	if err != nil {
 		s.log.Errorf("Failed to generate refresh token: %v", err)
-		return nil, status.Errorf(codes.Internal, "failed to generate refresh token")
+		return "", "", status.Errorf(codes.Internal, "failed to generate refresh token")
 	}
-	s.log.Debug("Refresh token generated")
 
-	// Get current time as the starting point
-	currentTime := time.Now()
-
-	// Calculate expiration time by adding token TTL to the current time
-	expiresAt := currentTime.Add(s.refreshTokenTTL)
-
-	// Create a Redis entry for the refresh session with user ID, token, and expiration time
+	// Create a Redis entry for the refresh session
+	expiresAt := time.Now().Add(s.refreshTokenTTL)
 	redisEntry := models.RefreshSession{
-		UserID:       in.UserId,        // User ID for whom the token is generated
-		RefreshToken: refreshToken,     // Generated refresh token
-		ExpiresAt:    expiresAt.Unix(), // Expiration time in Unix timestamp
+		UserID:       userId,
+		RefreshToken: refreshToken,
+		ExpiresAt:    expiresAt.Unix(),
 	}
 
 	// Inserting the entry to the redis
 	err = s.redisRepository.InsertRefreshSession(ctx, &redisEntry)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to insert refresh token to the redis storage")
+		return "", "", status.Errorf(codes.Internal, "failed to insert refresh token to the redis storage")
 	}
 
-	s.log.Debug("Successfully inserted refresh-token to the redis")
-
-	// Sending the response
-	s.log.Debug("Token generated successfully")
-	return &pb.TokenResponse{
-		AccessToken:  signedToken,
-		RefreshToken: refreshToken,
-	}, nil
+	return signedToken, refreshToken, nil
 }
